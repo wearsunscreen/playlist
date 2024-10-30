27acd8cf02c71827e305373038a41a84f3efe4bc
John Crane <john.crane@gmail.com>
2024-10-30 12:33:56 -0700

    Dark mode works

--- movie_library/routes.py
+++ movie_library/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, request, redirect
 
 
 pages = Blueprint(
@@ -12,3 +12,13 @@ def index():
         "index.html",
         title="Movies Watchlist",
     )
+
+@pages.get("/toggle_theme")
+def toggle_theme():
+    current_theme = session.get("theme")
+    if current_theme == "dark":
+        session["theme"] = "light"
+    else:
+        session["theme"] = "dark"
+
+    return redirect(request.args.get("current_page"))
