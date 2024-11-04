57424e6bfd63aaf563f0775ec06fde8df1ab8874
John Crane <john.crane@gmail.com>
2024-11-04 10:59:21 -0800

    Adding movie details, editing.

--- movie_library/forms.py
+++ movie_library/forms.py
@@ -1,6 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import InputRequired, NumberRange
+from wtforms import (
+    IntegerField,
+    StringField,
+    SubmitField,
+    TextAreaField,
+    URLField,
+)
+
+from wtforms.validators import (
+    InputRequired,
+    NumberRange,
+)
 
 
 class MovieForm(FlaskForm):
@@ -8,11 +18,36 @@ class MovieForm(FlaskForm):
     director = StringField("Director", validators=[InputRequired()])
 
     year = IntegerField(
-        "Year", 
+        "Year",
         validators=[
-            InputRequired(), 
-            NumberRange(min=1900, max=2028, message="Please enter a year in format YYYY.")
-        ]
+            InputRequired(),
+            NumberRange(min=1878, message="Please enter a year in the format YYYY."),
+        ],
     )
 
-    submit = SubmitField("Add Movie", validators=[InputRequired()])
+    submit = SubmitField("Add Movie")
+
+
+class StringListField(TextAreaField):
+    def _value(self):
+        if self.data:
+            return "\n".join(self.data)
+        else:
+            return ""
+
+    def process_formdata(self, valuelist):
+        # checks valuelist contains at least 1 element, and the first element isn't falsy (i.e. empty string)
+        if valuelist and valuelist[0]:
+            self.data = [line.strip() for line in valuelist[0].split("\n")]
+        else:
+            self.data = []
+
+
+class ExtendedMovieForm(MovieForm):
+    cast = StringListField("Cast")
+    series = StringListField("Series")
+    tags = StringListField("Tags")
+    description = TextAreaField("Description")
+    video_link = URLField("Video link")
+
+    submit = SubmitField("Submit")
--- movie_library/routes.py
+++ movie_library/routes.py
@@ -1,15 +1,18 @@
+import uuid
+import datetime
+from dataclasses import asdict
+
 from flask import (
     Blueprint,
+    current_app,
+    redirect,
     render_template,
     session,
-    request,
-    redirect,
-    current_app,
     url_for,
+    request,
 )
-from movie_library.forms import MovieForm
+from movie_library.forms import MovieForm, ExtendedMovieForm
 from movie_library.models import Movie
-from dataclasses import asdict
 
 
 pages = Blueprint(
@@ -21,7 +24,12 @@ pages = Blueprint(
 def index():
     movie_data = current_app.db.movie.find({})
     movies = [Movie(**movie) for movie in movie_data]
-    return render_template("index.html", title="Movies Watchlist", movies_data=movies)
+
+    return render_template(
+        "index.html",
+        title="Movies Watchlist",
+        movies_data=movies,
+    )
 
 
 @pages.route("/add", methods=["GET", "POST"])
@@ -30,24 +38,64 @@ def add_movie():
 
     if form.validate_on_submit():
         movie = Movie(
-            title=form.title.data, year=form.year.data, director=form.director.data
+            _id=uuid.uuid4().hex,
+            title=form.title.data,
+            director=form.director.data,
+            year=form.year.data,
         )
-        current_app.db.movie.insert_one(asdict(movie))
-        return redirect(url_for(".index"))
 
-    return render_template("new_movie.html", title="Add Movie", form=form)
+        current_app.db.movie.insert_one(asdict(movie))
+
+        return redirect(url_for(".movie", _id=movie._id))
+
+    return render_template(
+        "new_movie.html", title="Movies Watchlist - Add Movie", form=form
+    )
+
+
+@pages.route("/edit/<string:_id>", methods=["GET", "POST"])
+def edit_movie(_id: str):
+    movie = Movie(**current_app.db.movie.find_one({"_id": _id}))
+    form = ExtendedMovieForm(obj=movie)
+    if form.validate_on_submit():
+        movie.title = form.title.data
+        movie.director = form.director.data
+        movie.year = form.year.data
+        movie.cast = form.cast.data
+        movie.series = form.series.data
+        movie.tags = form.tags.data
+        movie.description = form.description.data
+        movie.video_link = form.video_link.data
+
+        current_app.db.movie.update_one({"_id": movie._id}, {"$set": asdict(movie)})
+        return redirect(url_for(".movie", _id=movie._id))
+    return render_template("movie_form.html", movie=movie, form=form)
 
 
 @pages.get("/movie/<string:_id>")
-def movie(_id):
-    movie_data = current_app.db.movie.find_one({"_id": _id})
-    if not movie_data:
-        about(404)
-    movie = Movie(**movie_data)
+def movie(_id: str):
+    movie = Movie(**current_app.db.movie.find_one({"_id": _id}))
     return render_template("movie_details.html", movie=movie)
 
 
-@pages.get("/toggle_theme")
+@pages.get("/movie/<string:_id>/watch")
+def watch_today(_id):
+    current_app.db.movie.update_one(
+        {"_id": _id}, {"$set": {"last_watched": datetime.datetime.today()}}
+    )
+
+    return redirect(url_for(".movie", _id=_id))
+
+
+@pages.get("/movie/<string:_id>/rate")
+def rate_movie(_id):
+    rating = int(request.args.get("rating"))
+    current_app.db.movie.update_one({"_id": _id}, {"$set": {"rating": rating}})
+
+    return redirect(url_for(".movie", _id=_id))
+
+
+@pages.get("/toggle-theme")
 def toggle_theme():
     current_theme = session.get("theme")
     if current_theme == "dark":
